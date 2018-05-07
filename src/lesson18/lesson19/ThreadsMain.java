16ca0075d674694e081a59b13a6f7fd473183088
Karachentsev Dmitry <xmitya@gmail.com>
2018-05-07 20:10:55 +0300

    Simple tread start

---
+++ src/lesson18/lesson19/ThreadsMain.java
@@ -0,0 +1,25 @@
+package lesson18.lesson19;
+
+public class ThreadsMain {
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < 5; i++) {
+            new Thread(new MyRunnable()).start();
+        }
+
+        System.out.println(Thread.currentThread().getName());
+    }
+
+    private static class MyThread extends Thread {
+        @Override
+        public void run() {
+            System.out.println("Hello! Thread is: " + getName());
+        }
+    }
+
+    private static class MyRunnable implements Runnable {
+        @Override
+        public void run() {
+            System.out.println("Hello, runnable! " + Thread.currentThread().getName());
+        }
+    }
+}
